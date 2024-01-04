e8618d0e8022fe2faced9180613383a6c00b3097
dOtOb9 <masa.taga0528@gmail.com>
2024-01-04 20:18:40 +0900

    first commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 
 sns.set_style("darkgrid")
 
-fields = np.linspace(0, 2 * np.pi)
+fields = np.linspace(0, 2 * np.pi, 1000)
 
 def sin(x):
     return np.sin(x)
@@ -16,19 +16,22 @@ data = sin(fields)
 
 data2 = cos(fields)
 
+data3 = sin(fields) * cos(fields)
+
 plt.plot(fields, data)
-ax = plt.plot(fields, data2)
+plt.plot(fields, data2)
+plt.plot(fields, data3)
 
 
-plt.title("Sin(x) and Cos(x)")
+plt.title("Sin(x), Cos(x) and Sin(x) * Cos(x)")
 
 plt.xlabel("x")
 plt.ylabel("y")
-fig = plt.legend(["Sin(x)", "Cos(x)"])
 
-plt.scatter([np.pi, np.pi], [np.sin(np.pi), np.cos(np.pi)], color="green")
+fig = plt.legend(["Sin(x)", "Cos(x)", "Sin(x) * Cos(x)"])
+
+plt.scatter([np.pi, np.pi], [np.sin(np.pi), np.cos(np.pi)], color="black")
 
 plt.plot([np.pi, np.pi], [np.sin(np.pi), np.cos(np.pi)], linestyle="--", color="black")
-
 
 plt.show()
