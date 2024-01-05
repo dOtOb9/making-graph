307d25e9fd5f2546e1222a29838a90e989a9d606
dOtOb9 <masa.taga0528@gmail.com>
2024-01-05 10:33:35 +0900

    add cos(x) + sin(x)

--- main.py
+++ main.py
@@ -18,20 +18,22 @@ data2 = cos(fields)
 
 data3 = sin(fields) * cos(fields)
 
+data4 = sin(fields) + cos(fields)
+
 plt.plot(fields, data)
 plt.plot(fields, data2)
 plt.plot(fields, data3)
+plt.plot(fields, data4)
 
-
-plt.title("Sin(x), Cos(x) and Sin(x) * Cos(x)")
+plt.title("Sin(x) and Cos(x)")
 
 plt.xlabel("x")
 plt.ylabel("y")
 
-fig = plt.legend(["Sin(x)", "Cos(x)", "Sin(x) * Cos(x)"])
+fig = plt.legend(["Sin(x)", "Cos(x)", "Sin(x) * Cos(x)", "Sin(x) + Cos(x)"])
 
 plt.scatter([np.pi, np.pi], [np.sin(np.pi), np.cos(np.pi)], color="black")
 
 plt.plot([np.pi, np.pi], [np.sin(np.pi), np.cos(np.pi)], linestyle="--", color="black")
 
-plt.show()
+plt.savefig("sin_cos.png")
